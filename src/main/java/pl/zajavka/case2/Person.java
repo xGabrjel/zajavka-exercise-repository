3cb00638da0bdafa0c4cdcb516080dd7cced51b4
xGabrjel <gabriel.luczyszyn@o2.pl>
2023-03-22 19:18:00 +0100

    Person class modification 3

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -4,6 +4,10 @@ import java.math.BigDecimal;
 
 record Person(String name, Long age, BigDecimal salary) {
 
+    public BigDecimal howMuchDoYouEarn() {
+        return salary;
+    }
+
     public Boolean doYouHaveABike() {
         return true;
     }
